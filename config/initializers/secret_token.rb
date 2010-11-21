0b2b32d8d847fb55f8202b7a62169ce3f722f8b6
Music <music@Ryan-Lanes-MacBook-Pro.local>
2010-11-21 02:41:07 -0700

    Initial commit that publishes the entire rails directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blp::Application.config.secret_token = 'e37c1ab3418a6be0776247de40dc0a39d439d1d91d337b27d0f516daa0b1ac21fbc76daad6b87b13c40815362d0bcbd2fa3cbb8a42f3d2cfb797c084c75faf33'
