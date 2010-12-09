b9b7a8494945f25ba814ac2c00d92af098fbdc7b
Music <music@Ryan-Lanes-MacBook-Pro.local>
2010-12-09 11:47:54 -0700

    Improved the structure of the Project unit tests

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,21 +1,21 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-	pt_id = 3
+	pt_film_id = Fixtures.identify(:film)
   test "First entry has a name" do
   	first = Project.first
     assert_not_nil first.name
   end
   test "Adding a new Project without a name should fail" do
-  	new = Project.create(:name => nil, :project_type_id => pt_id)
+  	new = Project.create(:name => nil, :project_type => ProjectType.find(pt_film_id))
   	assert !new.errors[:name].empty?
   end
   test "First entry has a project type" do
   	first = Project.first
   	assert_not_nil first.project_type_id
   end
-  test "Adding a new Project without a project_type_id should fail" do
-  	new = Project.create(:name => "Project Name", :project_type_id => nil)
+  test "Adding a new Project without a project_type should fail" do
+  	new = Project.create(:name => "Project Name", :project_type => nil)
   	assert !new.errors[:project_type_id].empty?
   	assert_nil new.project_type
   end
--- test/unit/project_type_test.rb
+++ test/unit/project_type_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class ProjectTypeTest < ActiveSupport::TestCase
-  test "First entry has a name" do
+  test "First ProjectType has a name" do
     one = ProjectType.first
     assert_not_nil one.name
   end
-  test "Adding a Project Type without a name should fail" do
+  test "Adding a ProjectType without a name should fail" do
   	newType = ProjectType.create(:name => nil)
   	assert !newType.errors[:name].empty?
+  end
+  test "First ProjectType has photos" do
+  	#assert ProjectType.first.projects
   end
 end
