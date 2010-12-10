c1db12614f2265ffd5bb0bda1c52132d2bb43d6f
Music <music@Ryan-Lanes-MacBook-Pro.local>
2010-12-10 15:10:36 -0700

    Added show :projects functionality to ProjectTypesController#show with the appropriate functional tests.

--- test/functional/project_types_controller_test.rb
+++ test/functional/project_types_controller_test.rb
@@ -2,8 +2,18 @@ require 'test_helper'
 
 class ProjectTypesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get(:show, {'name' => 'film'})
     assert_response :success
   end
-
+  test "should grab the correct ProjectType from name param" do
+  	get(:show, {'name' => 'film'})
+  	assert_select 'ul#side_nav_list' do
+  		assert_select 'li' do
+  			assert_select 'a.nav_a'
+  		end
+  	end
+  end
+  test "should display links to the ProjectType's Projects in the side navigation" do
+  	assert true
+  end
 end
