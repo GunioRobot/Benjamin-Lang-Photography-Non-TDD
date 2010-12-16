0a0e327a4e1fdd854143a30746dd7a7cc5a79cf4
Music <music@Ryan-Lanes-MacBook-Pro.local>
2010-12-16 13:54:35 -0700

    Fixed ProjectTypesController functional test names to match the actual test. Also wrote an actual test to 'display the correct ProjectType from the name param'

--- test/functional/project_types_controller_test.rb
+++ test/functional/project_types_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class ProjectTypesControllerTest < ActionController::TestCase
+  pt_film_id = Fixtures.identify(:film)
   test "should get show" do
     get(:show, {'name' => 'film'})
     assert_response :success
   end
-  test "should grab the correct ProjectType from name param" do
+  test "should display the correct ProjectType from name param" do
+  	get(:show, {'name' => 'film'})
+  	assert_select 'ul#side_nav_list' do |elements|
+  			assert_select elements.first, 'li' do
+  				assert_select 'a.nav_a', ProjectType.find(pt_film_id).projects.order('display_order').first.name
+  			end
+  	end
+  end
+  test "should display links to the ProjectType's Projects in the side navigation" do
   	get(:show, {'name' => 'film'})
   	assert_select 'ul#side_nav_list' do
   		assert_select 'li' do
   			assert_select 'a.nav_a'
   		end
   	end
-  end
-  test "should display links to the ProjectType's Projects in the side navigation" do
-  	assert true
+
   end
 end
