f47f17bcb6f58b96f98494f58cb221c842c12f3f
Music <music@Ryan-Lanes-MacBook-Pro.local>
2011-01-11 10:47:51 -0700

    Fixed a unit test operating under a legacy assumption.
    The photo_test.rb unit test tested for the presence of a title in the first Photo entry, a validation that is no longer in place. Fortunately, the test failed when it was run against the current photos fixtures. I also added a test for the presence of an img_url field in the first Photo entry. This new test passes.

--- test/unit/photo_test.rb
+++ test/unit/photo_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class PhotoTest < ActiveSupport::TestCase
   # Tests regarding existing Photo entries
   
-  test "First Photo has a title" do
-    assert_not_nil Photo.first.title
-  end
   test "First Photo belongs_to a Project" do
   	assert_not_nil Photo.first.project_id
   end
   test "First Photo has a display_order" do
     assert_not_nil Photo.first.display_order
+  end
+  test "First Photo has an img_url" do
+  	assert_not_nil Photo.first.img_url
   end
   
   # Tests regarding adding new Photo entries
@@ -37,4 +37,7 @@ class PhotoTest < ActiveSupport::TestCase
   	new = Photo.create(:title => 'newPhotoTitle', :img_url => nil, :display_order => next_display_order, :project => proj_one)
   	assert !new.errors[:img_url].empty?
   end
+  
+  # Tests regarding instance methods
+  
 end
