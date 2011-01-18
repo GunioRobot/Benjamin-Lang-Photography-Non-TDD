c8f19b00f63ce155274b98f041f11397ced1e208
Music <music@Ryan-Lanes-MacBook-Pro.local>
2011-01-18 12:29:01 -0700

    Updated Photo#show view and stylesheets along with iscellaneous additions and updates.
    This commit contains an updated Photo#show view using a <p> tag for the photo title rather than a <span>. Also, the stylesheets were updated with HTML element tags removed from class and ID declarations. The .photo_container now has a fixed height, which ensures that the 'prev'  and 'next' links don't move as a viewer clicks form image to image. This commit also contains loose testing files and fixtures that were left out of previous commits for whatever reason.

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,9 @@ class Project < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :project_type_id, :presence => true
 	validates :display_order, :presence => true
+	
+	def display_photos
+		return self.photos.order('display_order')
+	end
+	
 end
--- test/functional/photos_controller_test.rb
+++ test/functional/photos_controller_test.rb
@@ -24,4 +24,74 @@ class PhotosControllerTest < ActionController::TestCase
   		end
   	end
   end
+  test "should display the photo in a corresponding div" do
+  	photo = photos(:photo_one)
+    get(:show, {:id => photo.id})
+    assert_select 'div.photo_container' do
+    	assert_select 'img.photo' do
+    		assert_select '[src=?]', photo.img_url
+    		assert_select '[width=500]'
+    	end
+    end
+  end
+ 
+  # Conditional view functional tests
+  test "should display the photo's title in a span if the title exists" do
+  	photo = photos(:photo_one)
+    get(:show, {:id => photo.id})
+    assert_select 'div.photo_container' do
+    	assert_select 'span#photo_title', :text => photo.title
+    end
+  end
+  test "should NOT display a title if the photo doesn't have a title" do
+  	photo = photos(:photo_no_title)
+    get(:show, {:id => photo.id})
+    assert_select 'div.photo_container' do
+    	assert_select 'span#photo_title', false
+    end
+  end
+  test "should display the photo numbers div if the Photo's Project has more than one photo" do
+  	photo = photos(:photo_one)
+    get(:show, {:id => photo.id})
+    assert_not_equal photo.project.photos.length, 1
+    assert_select 'div#photo_numbers_div' do
+    	assert_select 'span#current_photo_number[class=photo_numbers]', :text => photo.project.display_photos.index(photo)+1
+    	assert_select 'span#total_photo_number[class=photo_numbers]', :text => photo.project.photos.length
+    end
+  end
+  test "should NOT display the photo numbers div if the Photo's Project has only photo" do
+  	photo = photos(:photo_solo)
+    get(:show, {:id => photo.id})
+    assert_equal photo.project.photos.length, 1
+    assert_select 'div#photo_numbers_div', false
+  end
+  test "should display a link to the previous photo if the Photo is not the first photo in the project" do
+  	photo = photos(:photo_three)
+    get(:show, {:id => photo.id})
+    assert_not_nil photo.prev_photo
+    assert_select 'a.prev_next_a', :text => 'prev' do
+    	assert_select '[href=?]', project_photo_path(photo.project,photo.prev_photo)
+    end
+  end
+  test "should NOT display a link to the previous photo if the Photo is the first photo in the project" do
+  	photo = photos(:photo_one)
+    get(:show, {:id => photo.id})
+    assert_nil photo.prev_photo
+    assert_select 'a.prev_next_a', {:text => 'prev', :count => 0}
+  end
+  test "should display a link to the next photo if the Photo is not the last photo in the project" do
+  	photo = photos(:photo_one)
+    get(:show, {:id => photo.id})
+    assert_not_nil photo.next_photo
+    assert_select 'a.prev_next_a', :text => 'next' do
+    	assert_select '[href=?]', project_photo_path(photo.project,photo.next_photo)
+    end
+  end
+  test "should NOT display a link to the next photo if the Photo is the last photo in the project" do
+  	photo = photos(:photo_solo)
+    get(:show, {:id => photo.id})
+    assert_nil photo.next_photo
+    assert_select 'a.prev_next_a', {:text => 'next', :count => 0}
+  end
+  
 end
