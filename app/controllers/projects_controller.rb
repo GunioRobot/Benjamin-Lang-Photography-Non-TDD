34968112140d444d02dd67233258dea3b0c33bb5
Music <music@Ryan-Lanes-MacBook-Pro.local>
2011-06-25 16:42:49 -0700

    Fleshed out dynamic Project#show view.
    The Project#show view now displays dynamically generated thumbnails that are hyperlinked to their corresponding photo's page. The appropriate 'first_thumb' and 'last thumb' class appendages are activated according to a super-basic algorithm that determines the column of the photo based on the order in which it is processed in the view.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   def show
   	@proj = Project.find(params[:id])
+  	@thumb_classes = ["thumb_container first_thumb","thumb_container","thumb_container last_thumb"]
   end
   
 end
